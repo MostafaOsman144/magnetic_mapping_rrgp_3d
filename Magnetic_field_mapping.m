79b72726f4b4cfd95736ea15b6193749950d1347
Mostafa Osman <M.E.A.Osman@tudelft.nl>
2021-06-04 10:19:44 +0200

    Implemented the batch estimation problem.
    Also, rearranged the main file.

--- Magnetic_field_mapping.m
+++ Magnetic_field_mapping.m
@@ -10,28 +10,47 @@
 
 clear; close all; clc;
 
+
+%% Configurable parameters needed in the script for the ease of testing
 load('measurements_aligned_simple_trajectory.mat'); % Simple scenario measurements
 % load('measurements_aligned_complicated_trajectory.mat'); % More complex scenario measurements
 
+downscale = 2; % Downsampling of the data for reducing running time and avoiding RAM overflow.
+training_set_factor = 70/100; % Percentage for dividing the dataset into test and training datasets for the batch estimation problem.
+
+% Initializing the GP hyperparameters.
+magnitude_scale_SE = 1;
+length_scale_SE = 0.3;
+magnitude_scale_lin = 1;
+measurement_noise = 0.1;
+
+space_margin = 10.0; % Defining the margin in the dirichlet boundary conditions.
+
+number_of_basis_functions = 500; % The number of basis functions for the approximation of the gram matrix.
+
+%% Organizing the data read from the data file. 
 sampling_time = T;
 magnetic_measurements = u(7:end, :);
 positions = p_opti;
 orientations = q_opti;
 gravity = g;
 
-downscale = 20;
 positions = positions(:, 1 : downscale : end);
 orientations = orientations(:, 1 : downscale : end);
 number_of_measurements = size(positions, 2);
 magnetic_measurements = magnetic_measurements(:, 1 : downscale : end);
 sampling_time = sampling_time * downscale;
 
-%% Initializing the hyperparameters of the Gaussian Process
-magnitude_scale_SE = 1;
-length_scale_SE = 0.3;
-magnitude_scale_lin = 1;
+% Forming the training and test datasets for the batch estimation problem.
+training_size = ceil(number_of_measurements * training_set_factor);
+test_size = floor(number_of_measurements * (1 - training_set_factor));
 
-space_margin = 10.0;
+positions_train = positions(:, 1 : training_size);
+positions_test = positions(:, training_size+1: end);
+magnetic_measurements_train = magnetic_measurements(:, 1 : training_size);
+magnetic_measurements_test = magnetic_measurements(:, training_size+1:end);
+
+%% Defining the Dirichlet boundary conditions of the eigendecomposition problem of the Gaussian Process
 x_u = max(positions(1, :)) + space_margin; % Upper bound of the X-coordinates 
 y_u = max(positions(2, :)) + space_margin; % Upper bound of the Y-coordinates
 z_u = max(positions(3, :)) + space_margin; % Upper bound of the Z-coordinates
@@ -43,23 +62,31 @@ z_l = abs(min(positions(3, :))) + space_margin; % Lower bound of the Z-coordinat
 boundaries = [max(x_u, x_l); 
               max(y_u, y_l); 
               max(z_u, z_l)]; % The boundaries of the eigendecomposition problem based on the actual boundaries of the data. 
-          
-number_of_basis_functions = 500;
 
 %% Compute the exact gram matrix for the GP prior
-exact_gram_matrix = calculateExactGramMatrix(positions, positions, magnitude_scale_lin, magnitude_scale_SE, length_scale_SE);
-
-%% Compute the approximated gram matrix for the GP prior using the eigenfunctions of the laplacian operator for the scalar potential
+fprintf('Time for calculating the exact gram matrix: \n');
 tic
-[pot_eigenfunctions, pot_eigenvalues] = calculatePotBasisFunctionsAndValues(positions, number_of_basis_functions, boundaries);
+exact_gram_matrix = calculateExactGramMatrix(positions_train, positions_train, magnitude_scale_lin, magnitude_scale_SE, length_scale_SE);
+toc
+%% Compute the approximated gram matrix for the GP prior using the eigenfunctions of the laplacian operator for the scalar potential
+fprintf('Time taken for calculating the Pot approximation: \n');
+tic
+[pot_eigenfunctions, pot_eigenvalues] = calculatePotBasisFunctionsAndValues(positions_train, number_of_basis_functions, boundaries);
 pot_spectral_eig_values = computeSpectralEigValsMat(pot_eigenvalues, magnitude_scale_SE, length_scale_SE, magnitude_scale_lin, 3);
 pot_approx_gram_mat = pot_eigenfunctions * pot_spectral_eig_values * pot_eigenfunctions';
 toc
 
 %% Compute the approximated gram matrix for the GP prior using the eigenfunctions of the laplacian operator for the Magnetic field
+fprintf('Time taken for calculating the Mag approximation: \n');
 tic
-[mag_eigenfunctions, mag_eigenvalues] = calculateMagBasisFunctionsAndValues(positions, number_of_basis_functions, boundaries);
+[mag_eigenfunctions, mag_eigenvalues] = calculateMagBasisFunctionsAndValues(positions_train, number_of_basis_functions, boundaries);
 mag_spectral_eig_values = computeSpectralEigValsMat(mag_eigenvalues, magnitude_scale_SE, length_scale_SE, magnitude_scale_lin, 3);
 mag_approx_gram_mat = mag_eigenfunctions * mag_spectral_eig_values * mag_eigenfunctions';
 toc
-%% Batch Estimation of the Magnetic field
+
+%% Batch Estimation for the Magnetic Field 
+fprintf('Time taken for calculating the Posterior of Magnetic field batch estimation problem: \n');
+tic 
+[mag_eigenfunctions_test, ~] = calculateMagBasisFunctionsAndValues(positions_test, number_of_basis_functions, boundaries);
+[mean_mag, cov_mag] = batchEstimation(mag_eigenfunctions, mag_spectral_eig_values, mag_eigenfunctions_test, magnetic_measurements_train, measurement_noise);
+toc
---
+++ batchEstimation.m
@@ -0,0 +1,14 @@
+%% Implementation of the batch estimation of the Gaussian Process regression. 
+
+function [mean, cov] = batchEstimation(eigenfunctions_training, spectral_eigenvalues, eigenfunctions_test, measurements, measurement_noise)
+m = size(spectral_eigenvalues, 1); % Number of basis functions
+d = size(measurements, 1); % number of dimensions of the measurement vector
+
+vec_measurements = reshape(measurements, size(measurements, 1) * size(measurements, 2), 1);
+
+mean = eigenfunctions_test * ((eigenfunctions_training' * eigenfunctions_training + (measurement_noise^2*eye(m)/spectral_eigenvalues)) \ eigenfunctions_training') * vec_measurements;
+cov = measurement_noise^2 * eigenfunctions_test * (eigenfunctions_training' * eigenfunctions_training + (measurement_noise^2*eye(m)/spectral_eigenvalues)) * eigenfunctions_test';
+
+number_of_training_positions = size(eigenfunctions_test, 1)/d;
+mean = reshape(mean, d, number_of_training_positions);
+end
